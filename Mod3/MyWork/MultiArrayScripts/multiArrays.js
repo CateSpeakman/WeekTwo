722d366ef3c6dda2122c57c4413cd9516cc35abb
Cate Speakman <speakmancate@gmail.com>
2019-07-31 15:25:28 -0400

    multiArray scripts

---
+++ Mod3/MyWork/MultiArrayScripts/multiArrays.js
@@ -0,0 +1,24 @@
+"use strict";
+
+//Description: This script will contain multidimensional arrays.
+//Author: Cate Speakman
+
+let teamMembers = [
+    ["Dana", "Brenda", "Happy"],
+    ["Laura", "Patti"],
+    ["Leslye", "Randy", "Mollye", "Ranse"],
+    ["Eloise", "Robert"]
+];
+
+let numTeams = teamMembers.length;
+
+for (let i = 0; i < numTeams; i++)
+{
+    console.log("Team " + i);
+
+    let numMembers = teamMembers[i].length;
+    for (let j = 0; j < numMembers; j++)
+    {
+        console.log(teamMembers[i][j]);
+    }
+}
